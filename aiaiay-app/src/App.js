9b346b7289ecef283f8fa8e01c60d6dcf28c1616
Eugenia Clarissa Anjani <e.clarissa.anjani@tum.de>
2022-04-23 17:32:38 +0200

    added files

--- aiaiay-app/src/App.js
+++ aiaiay-app/src/App.js
@@ -1,10 +1,25 @@
-var perf =require('./template.html');
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
+import ValueProp from './components/ValueProp';
+import MapVisual from './components/MapVisual';
 
-class Index extends React.Component {
-   render(){
-      return (
-         <iframe src={perf }></iframe>   /* like this */
-      );
-   }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
+export default function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
+      <ValueProp />
+    </div>
+  );
 }
-export default Index;
