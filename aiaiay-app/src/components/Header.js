8ca351ea716c3bdd6a45c3de0ed43f93eb7d215b
Eugenia Clarissa Anjani <e.clarissa.anjani@tum.de>
2022-04-23 17:31:20 +0200

    added files

---
+++ aiaiay-app/src/components/Header.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
+import SortIcon from '@material-ui/icons/Sort';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Link as Scroll } from 'react-scroll';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    fontFamily: 'Darker Grotesque',
+  },
+  appbar: {
+    background: 'none',
+  },
+  appbarWrapper: {
+    width: '80%',
+    margin: '0 auto',
+  },
+  appbarTitle: {
+    flexGrow: '1',
+  },
+  icon: {
+    color: '#fff',
+    fontSize: '2rem',
+  },
+  colorText: {
+    color: 'FFE5CC',
+  },
+  container: {
+    textAlign: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: '4.5rem',
+  },
+  goDown: {
+    color: 'FFE5CC',
+    fontSize: '4rem',
+  },
+}));
+export default function Header() {
+  const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, []);
+  return (
+    <div className={classes.root} id="header">
+      <AppBar className={classes.appbar} elevation={0}>
+        <Toolbar className={classes.appbarWrapper}>
+          <h1 className={classes.appbarTitle}>
+            brightly<span className={classes.colorText}>.ai</span>
+          </h1>
+          <IconButton>
+            <SortIcon className={classes.icon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Collapse
+        in={checked}
+        {...(checked ? { timeout: 1000 } : {})}
+        collapsedHeight={50}
+      >
+        <div className={classes.container}>
+          <h1 className={classes.title}>
+            Green electricity<br />
+            <span className={classes.colorText}>for everyone everywhere</span>
+          </h1>
+          <Scroll to="value-prop" smooth={true}>
+            <IconButton>
+              <ExpandMoreIcon className={classes.goDown} />
+            </IconButton>
+          </Scroll>
+        </div>
+      </Collapse>
+    </div>
+  );
+}
